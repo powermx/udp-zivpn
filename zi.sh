65e20950449e5badb5a3345c5ef2b98aa1323319
Roberto Jiménez <powermx@gmail.com>
2023-05-07 01:33:47 -0600

    UFW

--- zi.sh
+++ zi.sh
@@ -5,7 +5,7 @@
 echo -e "Updating server"
 sudo apt-get update && apt-get upgrade -y
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
-echo -e "Downloading Service"
+echo -e "Downloading UDP Service"
 wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
